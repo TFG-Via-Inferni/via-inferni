a94bceeab544f37f85c154ef3e26722bafcee5a7
Germán Sánchez Carmona <german.san.car@gmail.com>
2026-02-24 13:20:22 +0100

    feat: crear RoomType y RoomShape en Cell.cs (#1)

--- via-inferni/Assets/Scripts/Cell.cs
+++ via-inferni/Assets/Scripts/Cell.cs
@@ -1,13 +1,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum RoomType
+{
+    Regular,
+    Item,
+    Shop,
+    Boss,
+    Secret
+}
+
+public enum RoomShape
+{
+    OneByOne,
+    OneByTwo,
+    TwoByTwo,
+    TwoByTwo,
+    LShape
+}
+
 public class Cell : MonoBehaviour
 {
+    public RoomType roomType;
+    public RoomShape roomShape;
+
     public int index;
     public int value;
 
     public SpriteRenderer spriteRenderer;
     public SpriteRenderer roomSprite;
+
+    public List<int> cellList = new List<int>();
 
     public void SetSpecialRoomSprite(Sprite icon)
     {
@@ -17,6 +40,16 @@ public class Cell : MonoBehaviour
     public void SetRoomSprite(Sprite roomIcon)
     {
         roomSprite.sprite = roomIcon;
+    }
+
+    public void SetRoomType(RoomType newRoomType)
+    {
+        roomType = newRoomType;
+    }
+
+    public void SetRoomShape(RoomShape newRoomShape)
+    {
+        roomShape = newRoomShape;
     }
 
     public void RotateCell(List<int> connectedCells)
