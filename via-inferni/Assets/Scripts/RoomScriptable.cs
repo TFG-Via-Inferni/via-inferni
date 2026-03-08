b57bbc0e1f5593b4e22202eca7ce1f70502c8543
Adolfo Borrego <adolfo.bglez@gmail.com>
2026-03-08 13:19:51 +0100

    feat: añadir una sala nueva 1x1 y aádir colisiones a las paredes de puerta

--- via-inferni/Assets/Scripts/RoomScriptable.cs
+++ via-inferni/Assets/Scripts/RoomScriptable.cs
@@ -3,9 +3,14 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Room", menuName = "Scriptable Objects/Room")]
 public class RoomScriptable : ScriptableObject
 {
+    [Header("Room Configuration")]
     public RoomType roomType;
     public RoomShape roomShape;
 
+    [Header("Grid Ocupation")]
     public int[] occupiedTiles;
+    
+    [Header("Visual Variations")]
+    [Tooltip("Añade aquí múltiples prefabs de habitaciones con la misma forma pero diferente contenido (piedras, obstáculos, etc.)")]
     public GameObject[] roomVariations;
 }
